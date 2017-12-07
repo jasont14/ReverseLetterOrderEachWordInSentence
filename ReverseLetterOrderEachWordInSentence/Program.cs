32376df124663da23b5c2d68abbbc76bee834b6c
Jason Thatcher <contact@jasonthatcher.com>
2017-12-07 16:09:54 -0600

    Init

--- ReverseLetterOrderEachWordInSentence/Program.cs
+++ ReverseLetterOrderEachWordInSentence/Program.cs
@@ -10,6 +10,61 @@ namespace ReverseLetterOrderEachWordInSentence
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write Sentence: \n");
+            string a = Console.ReadLine();            
+                        
+            ReverseLetters(a);
+            Console.ReadLine();
         }
+
+        private static void ReverseLetters(string a)
+        {
+            string b = "";        
+            char c = '\u0020';
+            char[] punctuation =  {'.', '?', '!', ',', ';', ':'};
+            List<int> specials = new List<int>();
+            
+            int resume = 0;
+
+            for (int i = 0; i<a.Length; i++)
+            {               
+                //Assume space follows each word.
+                
+                if (c == a[i] | i == a.Length-1) //look for a space in the sentence or end of sentence.
+
+                {
+                    int stop = i;
+
+                    for (int k=i; k>resume-1; k--)  //Loop through current word to reverse letters and maintain punctuation
+                    {
+
+                        if (punctuation.Contains(a[k]))  //Punctuation?: Is the char found in punctuation array?  Save Char and sentence position so we can reinsert in proper position.
+                        {
+                            specials.Add(k);
+                        }
+                        else if (c != a[k])
+                        {
+                            b += a[k];  //If not add characters to string in reverse order.
+                        }
+                    }
+
+                    //add space back in
+                    b += " ";
+                                        
+                    resume = i;                                   
+                                      
+                    //find next character that is not a space.  Set resume location.
+                    
+                }                
+            }
+
+            //add punc back in at appropriate location
+            foreach (int p in specials)
+            {
+                b = b.Insert(p, Convert.ToString(a[p]));
+            }
+
+            Console.WriteLine("With letter order reversed: " + b);
+        }      
     }
 }
